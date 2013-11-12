3af1c747b73b60bcdb59b0402c4e6a814b9fd588
EdivanSilva <edivan37silva@gmail.com>
2013-11-12 19:33:31 -0200

    Adicionado Projeto1 Criado com eclipse.

---
+++ Projeto1/src/controle/PrimeiroProjetoGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package controle;
+
+/**
+ * @author edivan
+ *
+ */
+public class PrimeiroProjetoGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Este é um teste para funcionamento do git no eclipse!");
+	}
+
+}
